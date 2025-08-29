5d6ee10c1d20aedeb992481d8883fa0fcaf3d177
Enrico <gehring.enrico@first-soft-solutions.com>
2025-08-29 21:36:14 +0100

    Dokumentierte Version

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,23 @@
+/**
+ * Dieses Module erzeugt mit Hilfe von express Endpoints. S.g. Routes.
+ * 
+ * Es gibt zwei Endpoints:
+ * 
+ * ***1. Endpoint - POST (/Login)***
+ * ```text
+ * Dieser Endpoint nimmt eine E-Mail Adresse und ein Passwort entgegen. Der entsprechende Gegenpart wird aus der DB geholt
+ * und die Passwoerter verglichen. Sind die Passwoerter gleich, wird der Token erzeugt und von der API zurueckgegeben.
+ * Das erzeugen des Tokens wird im Modul ../utils/jwt.js vorgenommen.
+ * ```
+ * 
+ * ***2. Endpoint - POST (/verify)***
+ * ```text
+ * Dieser Endpoint nimmt einen Token entgegen und verifiziert diesen. Ist der Token verifiziert, so wird der Payload
+ * zurueckgegeben. Dies sind Informationen, die im Token hinterlegt sind. Hierfuer wird das Modul ../utils/jwt.js benutzt.
+ * ```
+ */
+const infoModule = "Beschreibung des Moduls";
+
 import { Router } from "express";
 import users from "../database.js";
 import { signToken, verifyToken } from "../utils/jwt.js";
@@ -6,10 +26,13 @@ import fs from 'fs';
 const router = new Router();
 
 /**
+ * Die Funktion schreibt unsere Logdatei. Die Logdatei wird im Rootverzeichnis abgelegt. Jede Zeile enthaelt einen TimeStamp:
+ * ***YYYYMMDD hh:mm;ss*** gefolgt von der Meldung.
  * 
- * @param {*} message 
+ * @param {*} message Die Meldung, die in die Logdatei geschrieben werden soll.
  */
 function writeLog(message) {
+    // Die Methode padStart fuellt den String von links mit dem String in Parameter 2 auf. Parameter 1 gibt an, wie lang der urspruengliche String sein soll.
     const pad = (n) => n.toString().padStart(2, '0');
 
     const date = new Date();
@@ -20,11 +43,13 @@ function writeLog(message) {
 }
 
 /**
+ * Diese Funktion vergleicht zwei Passwoerter. Sind diese identisch, so wird der Token erzeugt mit den entsprechenden
+ * uebergebenen Benutzerdaten (Parameter ***user***)
  * 
- * @param {*} pwd1 
- * @param {*} pwd2 
- * @param {*} user 
- * @returns 
+ * @param {*} pwd1 Passwort 1
+ * @param {*} pwd2 Passwort 2
+ * @param {*} user Die Benutzerdaten, die in den Token einfliessen sollen.
+ * @returns Der neu erstellte Token
  */
 function testPasswordAndSignToken(pwd1, pwd2, user) {
     if (pwd1 === pwd2) {
@@ -37,9 +62,17 @@ function testPasswordAndSignToken(pwd1, pwd2, user) {
 }
 
 /**
+ * Lesen der Benutzerdaten aus der Datenbank. Die Benutzerdatenbank ist hier fest hinterlegt.
+ * ***users***. Dies ist ein JSON-Array, gelesen aus dem Modul ***database.js***.
  * 
- * @param {*} userEmail 
- * @returns 
+ * Ist eine E-Mail gefunden wurden, muss auch ein zugehoeriges Passwort existieren.
+ * Falls nicht, wird undefined zurueckgegeben, ansonsten das gefundene ***JSON - Objekt*** mit
+ * den Benutzerdaten.
+ * 
+ * @param {*} userEmail Die E-Mail Adresse, die den Benutzer identifiziert.
+ * @returns undefined, wenn die o.g. Bedingungen nicht erfuellt sind oder kein Benutzer mit 
+ * zugehoeriger E-Mail Adresse gefunden wurde. Ansonsten das ***JSON - Objekt*** mit den 
+ * Benutzerdaten.
  */
 function searchUserByEmail(userEmail) {
     let retUser = undefined;
@@ -55,6 +88,17 @@ function searchUserByEmail(userEmail) {
 }
 
 /*
+ Der Endpoint (/login). Er nimmt eine E-Mail Adresse und ein Passwort entgegen.
+
+ {
+   "email": "nbiffen8@ycombinator.com",
+   "password": "jfmdkdir"
+ }
+
+ Anschliessend werden die Benutzerdaten an Hand der E-Mail Adresse aus der DB gelesen,
+ die Passwoerter verglichen und wenn diese uebereinstimmen wird der Token erstellt.
+
+ Der Token wird anschliessend an den Client zurueckgegeben.
 */
 router.post("/login", (req, res) => {
     console.log("login");
@@ -69,6 +113,11 @@ router.post("/login", (req, res) => {
 });
 
 /*
+ Der Endpoint (/verify). Er nimmt einen Token entgegen und verfiziert diesen. Stimmt der 
+ Token, so wird der Payload (Informationen zum Token) zurueckgegeben. 
+
+ Falls die Verifizierung ***undefined*** zurueckgibt, wird ***401 - unauthorized*** an den
+ Client zurueckgegeben.
 */
 router.post("/verify", (req, res) => {
     const token = req.body.token;
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,11 +1,31 @@
+/**
+ * Dieses Modul stellt Methoden zur Erstellung und Verifizierung eines Tokens bereit.
+ * 
+ * Hierfuer wird das Modul ***jsonwebtoken*** herangezogen.
+ * 
+ * Der Schluessel fuer den Token wird in der Datei ***.env*** gespeichert, welches mit
+ * mit dem Schluesselwort ***process*** gelesen wird.
+ * 
+ * Um die Umgebungsvariable in ***.env*** mit ***process*** auslesen zu koennen, wird
+ * das ***dotenv*** Modul benutzt.
+ */
+const infoModule = "Beschreibung des Moduls";
+
 import jsonwebtoken from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
+// Wichtig, um die Umgebungsvariable aus der ***.env*** Datei mit process lesen zu koennen.
 dotenv.config();
 
 /**
+ * Diese Methode erstellt einen Token und uebergibt an diesen die Benutzerdaten ***email***
+ * und ***_id***.
  * 
- * @param {*} user 
+ * Aktuell ist fest eingestellt, dass der Token 1h lang gueltig ist ***{ expiresIn: "1h" });***
+ * 
+ * Zurueckgegeben wird der erstellte Token.
+ * 
+ * @param {*} user Die Benutzerdaten.
  */
 export function signToken(user) {
     const payload = {};
@@ -18,9 +38,16 @@ export function signToken(user) {
 }
 
 /**
+ * Diese Methode verifiziert den uebergebenen Token. Dies wird in einer Try ... Catch - Anweisung
+ * ausgefuehrt. Ist der Token ungueltig, wirft ***jsonwebtoken.verify*** einen Fehler und das
+ * Programm bricht ab. Da das nicht gewollt ist, wird dies mit einer Try ... Catch - Anweisung
+ * abgefangen.
  * 
- * @param {*} token 
- * @returns 
+ * Ist der Token verifiziert wurden, so wird der ***Payload***, also die Informationen im Token
+ * zurueckgegeben.
+ * 
+ * @param {*} token Der Token, der verifiziert werden soll.
+ * @returns ***undefined***, falls die Verfifizierung fehl schlug, ansonsten den ***Payload***.
  */
 export function verifyToken(token) {
 
